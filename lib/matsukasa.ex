85783bb202a85dfa0a1a5b94a48246d8cf6a16ed
Yoshioka <sss.yoshioka@gmail.com>
2023-04-15 23:45:42 +0000

    initial commit

---
+++ lib/matsukasa.ex
@@ -0,0 +1,18 @@
+defmodule Matsukasa do
+  @moduledoc """
+  Documentation for `Matsukasa`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Matsukasa.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/matsukasa_test.exs
@@ -0,0 +1,8 @@
+defmodule MatsukasaTest do
+  use ExUnit.Case
+  doctest Matsukasa
+
+  test "greets the world" do
+    assert Matsukasa.hello() == :world
+  end
+end
